f1f834b522850f8183dc19da79954940c55075f1
Shama Khan <shama.k@media.net>
2020-07-20 02:22:07 +0530

    Add script to start server on heroku

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ const path = require('path');
 // const reactRenderer = require('./react-renderer');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // app.use('^/$', reactRenderer)
 
